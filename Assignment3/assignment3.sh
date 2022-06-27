41583c7867ffdce0257a9a365f1fedc9f8a627d2
Pascal Visser <p.visser@st.hanze.nl>
2022-06-27 13:28:56 +0200

    Fixed timings.txt typo

--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -6,4 +6,4 @@
 export DATA=/data/dataprocessing/MinIONData/all.fq
 export REF=/data/dataprocessing/MinIONData/all_bacteria.fna
 source /commons/conda/conda_load.sh
-for n in {1..16} ; do /usr/bin/time -o timings3.txt --append -f "${n}\t%e" minimap2  -a -t ${n} $REF $DATA > /dev/null ; done
+for n in {1..16} ; do /usr/bin/time -o timings.txt --append -f "${n}\t%e" minimap2  -a -t ${n} $REF $DATA > /dev/null ; done
