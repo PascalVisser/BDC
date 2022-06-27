6accce2bb5039c915e06ed5a47087d20612cae80
Pascal Visser <p.visser@st.hanze.nl>
2022-06-27 09:38:02 +0200

    fixed cpu-per-task error

--- Assignment3/assignment3.sh
+++ Assignment3/assignment3.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#SBATCH --time 2:00:00
+#SBATCH --time 72:00:00
 #SBATCH --nodes=1
-#SBATCH --cpus_per_task=16
+#SBATCH --cpus-per-task=16
 #SBATCH --partition=assemblix
 export DATA=/data/dataprocessing/MinIONData/all.fq
 export REF=/data/dataprocessing/MinIONData/all_bacteria.fna
-for n in {1..16} ; do /usr/bin/time -o timings3.txt --append -f "${n}\t%e" Minimap2  -a -t ${n} $REF $DATA > /dev/null ; done
+source /commons/conda/conda_load.sh
+for n in {1..16} ; do /usr/bin/time -o timings3.txt --append -f "${n}\t%e" minimap2  -a -t ${n} $REF $DATA > /dev/null ; done
